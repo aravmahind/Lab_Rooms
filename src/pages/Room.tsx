c27d582a17f4462ec1b2c49cb3313c01541ab924
Chirag chaudhari <100666926+Chiragchaudhari2005@users.noreply.github.com>
2025-07-25 19:12:50 +0530

    Update Room.tsx

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import CodeEditor from '@uiw/react-textarea-code-editor'
 import { Button } from '../components/ui/button'
 
